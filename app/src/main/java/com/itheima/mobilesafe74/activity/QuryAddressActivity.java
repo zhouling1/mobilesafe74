406049f6154710ce4a50798c13323864465e0f07
zhouling <1079974929@qqcom>
2017-06-12 18:18:57 +0800

    写到QuryAddressActivity

--- app/src/main/java/com/itheima/mobilesafe74/activity/QuryAddressActivity.java
+++ app/src/main/java/com/itheima/mobilesafe74/activity/QuryAddressActivity.java
@@ -2,6 +2,12 @@ package com.itheima.mobilesafe74.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.itheima.mobilesafe74.R;
 
@@ -10,10 +16,30 @@ import com.itheima.mobilesafe74.R;
  */
 
 public class QuryAddressActivity extends Activity{
+    private EditText ed_Phone;
+    private Button btn_query;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_address);
+        initView();
+    }
+
+    private void initView() {
+        ed_Phone= (EditText) findViewById(R.id.et_phone);
+        btn_query= (Button) findViewById(R.id.bt_query);
+        btn_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(ed_Phone.getText())){
+                    //query();
+                }else {
+                    Animation shake= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.shake);
+
+                    ed_Phone.startAnimation(shake);
+                }
+            }
+        });
 
     }
 }
